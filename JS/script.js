1776eb5252236e7170d46cda4688bba1b7157ce3
miguel murua <murua.miguel91@gmail.com>
2023-03-08 14:49:57 -0300

    Primer commit

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+/*================== typing animation ======================*/
+
+const typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
