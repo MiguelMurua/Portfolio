1b2d2b56287ea9901ab3c0b43fa691cee7ae42ab
miguel murua <murua.miguel91@gmail.com>
2023-03-08 21:03:05 -0300

    cuarto commit

--- JS/script.js
+++ JS/script.js
@@ -73,6 +73,8 @@ document.querySelector(".hire-me").addEventListener("click", function(){
     const sectionIndex = this.getAttribute("data-section-index");
     showSection(this);
     updateNav(this);
+    removeBackSection();
+    addBackSection(sectionIndex)
 })
 const navTogglerBtn = document.querySelector(".nav-toggler"),
     aside = document.querySelector(".aside");
